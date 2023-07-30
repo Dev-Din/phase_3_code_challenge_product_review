2f62825ca27cffac3075e68e213331404f9d44d2
nurdinhajo <nurudiin222@gmail.com>
2023-07-30 18:25:32 +0300

    Complete Assignment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
-    
+ has_many :reviews
+ has_many :products, through: :reviews
+
+ def favorite_product
+  self.products.order('star_rating').last #returns the product instance
+ end
+
+ def remove_reviews(product)
+  self.reviews.where(product: product).destroy_all
+ end
 end
